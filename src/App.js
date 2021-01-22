ba0cb4494cb0fc35532e1cb60cff98c66e14b50c
Adam Kent <adamkentdev@gmail.com>
2021-01-22 10:21:03 -0700

    set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userInput: '',
+      data: []
+    }
+  }
+
+  changeHandler(val){
+    this.setState({userInput: val})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input 
+          onChange={ e => this.changeHandler(e.target.value)} 
+          value={this.state.userInput} 
+          type="text" 
+          placeholder="yada yada yada"/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = 
+[
+    {
+        id: 1,
+        name: {
+            first: 'Adam',
+            last: 'Kent'
+        },
+        favGiantRobot: 'Voltron'
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Ben',
+            last: 'Dehlin'
+        },
+        favGiantRobot: 'Megazord'
+    },
+    {
+        id: 1,
+        name: {
+            first: 'Cole',
+            last: 'Finlayson'
+        },
+        favGiantRobot: 'Shrek'
+    }
+]
+
+export default data;
